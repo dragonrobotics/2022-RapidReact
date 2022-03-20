d1f9eae913cfe7a52b3d68f4bf94ec6d51f37304
Mattie Nash <mnash@kittymail.com>
2022-03-20 14:36:26 -0500

    shooter code

--- src/main/java/team_5002/robot/systems/Shooter.java
+++ src/main/java/team_5002/robot/systems/Shooter.java
@@ -1,35 +1,45 @@
 package team_5002.robot.systems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.platform.DeviceType;
 
+import team_5002.robot.Robot;
 import team_5002.robot.libraries.Vision;
+import team_5002.robot.libraries.controls;
 
 public class Shooter {
-    WPI_TalonSRX motor1;
-    WPI_TalonSRX belt1;
-    WPI_TalonSRX belt2;
-    WPI_TalonSRX belt3;
-    public Shooter(WPI_TalonSRX motor1, WPI_TalonSRX belt1, WPI_TalonSRX belt2, WPI_TalonSRX belt3){
-        this.motor1 = motor1;
-        this.belt1 = belt1;
-        this.belt2 = belt2;
-        this.belt3 = belt3;
-    }
-    public double computeSpeed(double shooterAngle) {
+    double shooterAngle = 0;
+    WPI_TalonSRX motor1 = (WPI_TalonSRX) Robot.Devices.getDevice("shooter1");
+    WPI_TalonSRX motor2 = (WPI_TalonSRX) Robot.Devices.getDevice("shooter2");
+    WPI_TalonSRX belt1 = (WPI_TalonSRX) Robot.Devices.getDevice("belt1");
+    WPI_TalonSRX belt2 = (WPI_TalonSRX) Robot.Devices.getDevice("belt2");
+    WPI_TalonSRX belt3 = (WPI_TalonSRX) Robot.Devices.getDevice("belt3");
+    public Shooter(WPI_TalonSRX motor1, WPI_TalonSRX belt1, WPI_TalonSRX belt2, WPI_TalonSRX belt3){}
+    public double computeSpeed() {
         double distance = Vision.determineObjectDist();
         return Math.sqrt((16.1 * Math.pow(distance, 2)) / (distance * Math.tan(shooterAngle) - Vision.camAngle - Vision.goalHeight)) / Math.cos(shooterAngle);
       }
-    public double getShooterSpeed(){
-        return 0;
+    private double getShooterSpeed(){
+        return motor1.getSelectedSensorVelocity()/1024;
     }
-    public void setShooterSpeed(double speed){
+    private void run(double speed){
         this.motor1.set(speed);
+        this.motor2.set(speed);
     }
-    public void shooterPeriodic(){
-
+    private void setBeltSpeed(double speed){
+        this.belt1.set(speed);
+        this.belt2.set(speed);
+        this.belt3.set(speed);
     }
 
-    public void setBeltSpeed(double speed){
-        this.belt1.set(-speed);
+    public void loop(){
+        if((boolean) Robot.Controls.getInput("Shoot") || (boolean) Robot.Controls.getInput("Ait")){
+            if((boolean) Robot.Controls.getInput("Shoot")){
+                if(Vision.canSeeTarget()){
+                    //if(Math.abs(Vision.aim()))
+                }
+            }
+        }
+
     }
 }
