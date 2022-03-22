e1cafe0acb885cdf19232f4802d7d44dd109ae57
hailey-7113 <73400331+hailey-7113@users.noreply.github.com>
2022-03-21 19:05:10 -0600

    make lift functional

--- src/main/java/team_5002/robot/systems/Lift.java
+++ src/main/java/team_5002/robot/systems/Lift.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 import team_5002.robot.Robot;
@@ -12,49 +13,63 @@ import team_5002.robot.libraries.controls;
 
 
 public class Lift {
-    static WPI_TalonSRX liftMotor;
-    static Pneumatics pneumatics;
-    static controls controls = Robot.Controls;
-    static boolean button7 = (boolean) controls.getInput("button7");
-    static boolean button8 = (boolean) controls.getInput("button8");
-    static boolean button9 = (boolean) controls.getInput("button9");
-    static boolean button10 = (boolean) controls.getInput("button10");
+    WPI_TalonSRX liftMotor;
+    Pneumatics pneumatics;
+    controls controls = Robot.Controls;
+    boolean up = (boolean) controls.getInput("up");
+    boolean down = (boolean) controls.getInput("down");
+    boolean lean = (boolean) controls.getInput("lean");
+    boolean straighten = (boolean) controls.getInput("straighten");
+    DigitalInput limitSwitch1 = new DigitalInput(5);
+    DigitalInput limitSwitch2 = new DigitalInput(6);
     public Lift(){
         liftMotor = (WPI_TalonSRX) Robot.Devices.getDevice("liftMotor");
         DoubleSolenoid[] solenoids = {(DoubleSolenoid) Robot.Devices.getDevice("solenoid1"),(DoubleSolenoid) Robot.Devices.getDevice("solenoid2")};
         this.pneumatics = new Pneumatics(solenoids, (Compressor) Robot.Devices.getDevice("compressor"));
     }
 
-    public static void armUp(){
-        if(button7){
-            liftMotor.set(.5);
+    public void armUp(){
+        if(limitSwitch1.get() == false){
+            this.liftMotor.set(.5);
+        } else {
+            armStop();
         }
     }
 
-    public static void armDown(){
-        if(button8){
-            liftMotor.set(-.5); 
+    public void armDown(){   
+        if(limitSwitch2.get() == false){
+            this.liftMotor.set(-.5);
+        } else {
+            armStop();
         }
     }
 
-    public static void armStop(){
-        liftMotor.set(0);
-        liftMotor.setNeutralMode(NeutralMode.Brake);
+    public void armStop(){
+        this.liftMotor.set(0);
+        this.liftMotor.setNeutralMode(NeutralMode.Brake);
     }
 
-    public static void lean(){
-        if(button9){
-            pneumatics.open();
-        }
+    public void lean(){
+            this.pneumatics.open();
     }
 
-    public static void straighten(){
-        if(button10){
-            pneumatics.close();   
-        }
+    public void straighten(){
+            this.pneumatics.close();   
     }
 
     public void loop(){
+        if(up){
+            armUp();
+        }
+        if(down){
+            armDown();
+        }
+        if(lean){
+            lean();
+        }
+        if(straighten){
+            straighten();
+        }
         
     }
 }
