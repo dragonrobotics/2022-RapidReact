49ffbb8f7aa00b235e00d1d74496a128b5406557
Mattie Nash <mnash@kittymail.com>
2022-09-10 10:32:01 -0500

    ShortShot consistency

--- src/main/java/frc/robot/commands/Shooter/ShortShot.java
+++ src/main/java/frc/robot/commands/Shooter/ShortShot.java
@@ -17,7 +17,7 @@ public class ShortShot extends CommandBase{
     public void execute(){
         m_intake.startBelt();
         m_intake.startFeed();
-        m_shooter.setPower(1);
+        m_shooter.setSpeed(200);
     }
 
     @Override
