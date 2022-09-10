016eebff761065ffeca596328a73b30d9f315c93
Mattie Nash <mnash@kittymail.com>
2022-09-10 12:21:15 -0500

    Fix short shot

--- src/main/java/frc/robot/commands/Shooter/ShortShot.java
+++ src/main/java/frc/robot/commands/Shooter/ShortShot.java
@@ -1,5 +1,8 @@
 package frc.robot.commands.Shooter;
 
+import javax.swing.text.StyleContext.SmallAttributeSet;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.Shooter;
@@ -11,13 +14,21 @@ public class ShortShot extends CommandBase{
         m_intake = intake;
         m_shooter = shooter;
         addRequirements(intake, shooter);
-    }   
+    }
 
     @Override
     public void execute(){
-        m_intake.startBelt();
-        m_intake.startFeed();
-        m_shooter.setSpeed(200);
+        double speed = SmartDashboard.getNumber("ShortSpeed", 200);
+        m_shooter.setSpeed(speed);
+        if(Math.abs(m_shooter.getSpeed()-speed) < 10){
+            m_intake.startBelt();
+            m_intake.startFeed();
+            m_intake.startIntake();
+        }else{
+            m_intake.stopBelt();
+            m_intake.stopFeed();
+            m_intake.stopIntake();
+        }
     }
 
     @Override
